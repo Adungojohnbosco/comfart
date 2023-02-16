d312c938c3a76cc74320f46ea013872b0d1ac41a
Adungojohnbosco <96256381+Adungojohnbosco@users.noreply.github.com>
2023-02-16 02:42:42 -0800

    Initial commit

---
+++ src/main/kotlin/grading system.kt
@@ -0,0 +1,9 @@
+//import java.util.*
+import java.util.Scanner
+fun main(args: Array<String>) {
+        print("Enter what yourr want: ")
+
+        val DoubleInput = readLine()!!
+        println("You entered: $DoubleInput")
+
+}
